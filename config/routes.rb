c14b7542708cbe4401ee421e6a6b3f392645f040
Gustavo Scanferla <gustavo@scanfer.la>
2025-09-24 20:52:51 -0300

    Minor tweak

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  scope defaults: { format: :json } do
-    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-    # Can be used by load balancers and uptime monitors to verify that the app is live.
-    get "up" => "rails/health#show", as: :rails_health_check
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
 
+  scope defaults: { format: :json } do
     resources :providers, only: [] do
       resources :availabilities, only: :index, module: :providers
     end
