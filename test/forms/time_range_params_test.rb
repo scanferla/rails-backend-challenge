a9fa8e40baf6414c92da210f25e333cbdac43b66
Gustavo Scanferla <gustavo@scanfer.la>
2025-09-24 23:11:12 -0300

    Minor tweak

--- test/forms/time_range_params_test.rb
+++ test/forms/time_range_params_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class TimeRangeParamsTest < ActiveSupport::TestCase
   test "valid with proper ISO8601 from/to and to after from" do
     params = TimeRangeParams.new(from: "2025-10-06T09:00:00Z", to: "2025-10-06T10:00:00Z")
+
     assert params.valid?
     assert_instance_of Time, params.from
     assert_instance_of Time, params.to
@@ -17,6 +18,7 @@ class TimeRangeParamsTest < ActiveSupport::TestCase
 
   test "invalid when to is not after from" do
     params_equal = TimeRangeParams.new(from: "2025-10-06T10:00:00Z", to: "2025-10-06T10:00:00Z")
+
     assert_not params_equal.valid?
     assert params_equal.errors.of_kind?(:to, :greater_than)
   end
@@ -25,18 +27,21 @@ class TimeRangeParamsTest < ActiveSupport::TestCase
     past_from = 1.day.ago.iso8601
     future_to = 1.hour.from_now.iso8601
     params_clamped = TimeRangeParams.new(from: past_from, to: future_to)
+
     assert params_clamped.valid?
     assert params_clamped.from >= Time.zone.now - 1.second # allow tiny drift
   end
 
   test "to must be in the future (implicit via to > clamped from)" do
     params_with_to_before_from = TimeRangeParams.new(from: 1.minute.from_now.iso8601, to: 1.second.from_now.iso8601)
+
     assert_not params_with_to_before_from.valid?
     assert params_with_to_before_from.errors.of_kind?(:to, :greater_than)
   end
 
   test "when only from is missing shows presence error only" do
     params_only_to = TimeRangeParams.new(from: nil, to: 1.hour.from_now.iso8601)
+
     assert_not params_only_to.valid?
     assert params_only_to.errors.of_kind?(:from, :blank)
     refute params_only_to.errors.of_kind?(:to, :greater_than)
@@ -44,6 +49,7 @@ class TimeRangeParamsTest < ActiveSupport::TestCase
 
   test "when only to is missing shows presence error only" do
     params_only_from = TimeRangeParams.new(from: 1.hour.from_now.iso8601, to: nil)
+
     assert_not params_only_from.valid?
     assert params_only_from.errors.of_kind?(:to, :blank)
     refute params_only_from.errors.of_kind?(:to, :greater_than)
